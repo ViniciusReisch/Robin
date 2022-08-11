18d93deb644ce295f54ab745f36b01d50d12a590
Vinícius Reisch <97248458+Flyinng@users.noreply.github.com>
2022-08-11 16:53:42 -0300

    Introducing data price and name in dictionaries

--- src/web-scraping/data-pieces/pichau/ram/8gb.py
+++ src/web-scraping/data-pieces/pichau/ram/8gb.py
@@ -1,33 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+import re
+import csv
 
-# Memória ram 8gb
+# Memória ram 8gb DDR4
 namesProducts = []
 header = {'user-agent': 'Mozilla/5.0'}
-response = requests.get("https://www.pichau.com.br/hardware/memorias?capacidadememoria=199", headers=header)
+response = requests.get("https://www.pichau.com.br/hardware/memorias?capacidadememoria=199&tipo_de_memoria=422", headers=header)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-for nome in soup.select('.MuiTypography-h6'):
-    namesProducts.append(nome.text)
+for name in soup.select('.MuiTypography-h6'):
+    namesProducts.append(name.text)
 
 navegador = webdriver.Chrome()
-navegador.get('https://www.pichau.com.br/hardware/memorias?capacidadememoria=199')
-produto = navegador.find_elements('class name', 'jss69')
+navegador.get('https://www.pichau.com.br/hardware/memorias?capacidadememoria=199&tipo_de_memoria=422')
+product = navegador.find_elements('class name', 'jss191')
 priceProducts = []
-sleep(5)
-for i in produto:
+sleep(2)
+for i in product:
     if i.text == "":
         continue
     priceProducts.append(i.text)
     sleep(1)
 
+allLinkProducts = []
+linkProducts = navegador.find_element_by_xpath('//*[@id="__next"]/main/div[2]/div/div[1]/div[2]/div[1]/a')
+sleep(2)
+for i in linkProducts:
+    if i.text == "":
+        continue
+    allLinkProducts.append(i.text)
+    sleep(1)
+print(allLinkProducts)
+
 # Reunindo dados em json
 allData = []
 for i in range(len(priceProducts)):
-    dataDic = {namesProducts[i]: priceProducts[i]}
+    if len(priceProducts[i]) >= 10:
+        priceProducts[i] = priceProducts[i].replace('.', '')
+    changeablePriceProducts = priceProducts[i].replace('R$', '').replace(',', '.')
+    dataDic = {
+        "Name": namesProducts[i],
+        "Price": [priceProducts[i], float(changeablePriceProducts)]
+    }
     allData.append(dataDic)
-
 print(allData)
+
