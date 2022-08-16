c624d0ebdf020cbde7132003a3a257a22bd1776e
Vinícius Reisch <97248458+Flyinng@users.noreply.github.com>
2022-08-16 10:41:25 -0300

    Continuing scraping at Pichau

--- src/web-scraping/data-pieces/pichau/ram/8gb.py
+++ src/web-scraping/data-pieces/pichau/ram/8gb.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 namesProducts = []
 imgProducts = []
 allData = []
+installmentPriceProducts = []
 linksProducts = []
 pricesProducts = []
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'}
@@ -18,23 +19,26 @@ soup = BeautifulSoup(response.text, 'html.parser')
 for nome in soup.select('.MuiTypography-h6'):
     namesProducts.append(nome.text)
 
-images = soup.select ('img')
-print(images)
-
 driver = webdriver.Chrome()
 driver.get('https://www.pichau.com.br/hardware/memorias?capacidadememoria=199&tipo_de_memoria=422')
-produto = driver.find_elements('class name', 'jss191')
+produto = driver.find_elements('class name', 'jss64')
 for i in produto:
     if i.text == "":
         continue
     pricesProducts.append(i.text)
+
+produto = driver.find_elements('class name', 'jss72')
+for i in produto:
+    if i.text == "":
+        continue
+    installmentPriceProducts.append(i.text)
 
 links = driver.find_elements('tag name', 'a')
 for i in links:
     if 'memoria' in i.get_attribute('href'):
         linksProducts.append(i.get_attribute('href'))
 
-# linksImg = driver.find_elements('class name', 'jss177')
+# linksImg = driver.find_elements('tag name', 'img')
 # print(linksImg)
 # for i in linksImg:
 #     imgProducts.append(i.get_attribute('src'))
@@ -44,6 +48,9 @@ for i in range(len(pricesProducts)):
     if '.' in pricesProducts[i]:
         pricesProducts[i] = pricesProducts[i].replace('.', '')
     changeablePrices = pricesProducts[i].replace('R$', '').replace(',', '.')
-    dataDic = {'Name': namesProducts[i], 'Price': [pricesProducts[i], float(changeablePrices)], 'Link': linksProducts[i], 'Img': imgProducts[i]}
+    if '.' in installmentPriceProducts[i]:
+        installmentPriceProducts[i] = installmentPriceProducts[i].replace('.', '')
+    changeableInstallmentPriceProducts = installmentPriceProducts[i].replace('R$', '').replace(',', '.')
+    dataDic = {'Name': namesProducts[i], 'Price': [pricesProducts[i], float(changeablePrices)], 'Installment price': [installmentPriceProducts[i], float(changeableInstallmentPriceProducts)] , 'Link': linksProducts[i]}
     allData.append(dataDic)
 print(allData)
