ed494e9d5b7c3ca4da55db056741e98d09e4c2c9
Vinícius Reisch <97248458+Flyinng@users.noreply.github.com>
2022-08-19 11:48:41 -0300

    Initializing Filtres

--- src/web-scraping/data-pieces/pichau/ram/8gb.py
+++ src/web-scraping/data-pieces/pichau/ram/8gb.py
@@ -9,113 +9,119 @@ allData = []
 installmentPriceProducts = []
 linksProducts = []
 pricesProducts = []
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'}
-response = requests.get("https://www.pichau.com.br/hardware/memorias?capacidadememoria=199&tipo_de_memoria=422",
-                        headers=header)
-soup = BeautifulSoup(response.text, 'html.parser')
 
-for nome in soup.select('.MuiTypography-h6'):
-    namesProducts.append(nome.text)
+for i in range(10):
+    driver = webdriver.Chrome()
+    page = i + 1
+    link = 'https://www.pichau.com.br/hardware/memorias?page='
+    driver.get(link + str(page))
 
-driver = webdriver.Chrome()
-driver.get('https://www.pichau.com.br/hardware/memorias?capacidadememoria=199&tipo_de_memoria=422')
+    # Crawling Products == Image
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 500);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 1000);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 1500);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 2000);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 2500);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 3000);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 3500);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 4000);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 4500);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 5000);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    imgProducts = list(dict.fromkeys(imgProducts))
+    driver.execute_script("window.scrollTo(0, 5500);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 6000);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 6300);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    driver.execute_script("window.scrollTo(0, 6700);")
+    product = driver.find_elements('tag name', 'img')
+    for i in product:
+        if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(i.get_attribute('src'))
+    imgProducts = list(dict.fromkeys(imgProducts))
 
-# Crawling Products == Image
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 500);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 1000);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 1500);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 2000);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 2500);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 3000);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 3500);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 4000);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 4500);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 5000);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-imgProducts = list(dict.fromkeys(imgProducts))
-driver.execute_script("window.scrollTo(0, 5500);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 6000);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-driver.execute_script("window.scrollTo(0, 6300);")
-product = driver.find_elements('tag name', 'img')
-for i in product:
-    if 'product' in i.get_attribute('src'):  # Only separate images with product in the name
-        imgProducts.append(i.get_attribute('src'))
-imgProducts = list(dict.fromkeys(imgProducts))
+    # Crawling Products == Price
+    product = driver.find_elements('class name', 'jss69')
+    for i in product:
+        if 'R$' in i.text:
+            pricesProducts.append(i.text)
 
-# Crawling Products == Price
-product = driver.find_elements('class name', 'jss69')
-for i in product:
-    if i.text == "":
-        continue
-    pricesProducts.append(i.text)
+    # Crawling Products == Installment Price
+    product = driver.find_elements('class name', 'jss77')
+    for i in product:
+        if 'R$' in i.text:
+            installmentPriceProducts.append(i.text)
 
-# Crawling Products == Installment Price
-product = driver.find_elements('class name', 'jss77')
-for i in product:
-    if i.text == "":
-        continue
-    installmentPriceProducts.append(i.text)
+    # Crawling Products == Name
+    product = driver.find_elements('tag name', 'h2')
+    for i in product:
+        if i.text == "":
+            continue
+        namesProducts.append(i.text)
 
-# Crawling Products == Links
-links = driver.find_elements('tag name', 'a')
-for i in links:
-    if 'memoria' in i.get_attribute('href'):
-        linksProducts.append(i.get_attribute('href'))
-driver.close()
+    # Crawling Products == Links
+    links = driver.find_elements('tag name', 'a')
+    for i in links:
+        if 'memoria' in i.get_attribute('href'):
+            linksProducts.append(i.get_attribute('href'))
+    driver.close()
 
 # Separating data in dictionary for better reading
-for i in range(len(pricesProducts)):
+for i in range(len(installmentPriceProducts)):
     if '.' in pricesProducts[i]:
         pricesProducts[i] = pricesProducts[i].replace('.', '')
     changeablePrices = pricesProducts[i].replace('R$', '').replace(',', '.')
@@ -126,4 +132,39 @@ for i in range(len(pricesProducts)):
                'Installment price': [installmentPriceProducts[i], float(changeableInstallmentPriceProducts)],
                'Link': linksProducts[i], 'Image': imgProducts[i]}
     allData.append(dataDic)
-print(allData)
+
+    # FILTER == 8gb 3200mhz
+    data8gb3200mhz = []
+    for data in allData:
+        if '(1x8GB)' in data['Name'] and '3200MHz' in data['Name']:
+            data8gb3200mhz.append(data)
+
+    # FILTER == 8gb 3000mhz
+    data8gb3000mhz = []
+    for data in allData:
+        if '(1x8GB)' in data['Name'] and '3200MHz' in data['Name']:
+            data8gb3200mhz.append(data)
+
+    # FILTER == 8gb 3200mhz
+    data8gb3200mhz = []
+    for data in allData:
+        if '(1x8GB)' in data['Name'] and '3200MHz' in data['Name']:
+            data8gb3200mhz.append(data)
+
+    # FILTER == 8gb 3200mhz
+    data8gb3200mhz = []
+    for data in allData:
+        if '(1x8GB)' in data['Name'] and '3200MHz' in data['Name']:
+            data8gb3200mhz.append(data)
+
+    # FILTER == 8gb 3200mhz
+    data8gb3200mhz = []
+    for data in allData:
+        if '(1x8GB)' in data['Name'] and '3200MHz' in data['Name']:
+            data8gb3200mhz.append(data)
+
+    # FILTER == 8gb 3200mhz
+    data8gb3200mhz = []
+    for data in allData:
+        if '(1x8GB)' in data['Name'] and '3200MHz' in data['Name']:
+            data8gb3200mhz.append(data)
