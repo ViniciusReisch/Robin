a26209c553e286066a7cea13e275cd6194045c58
Vinícius Reisch <97248458+Flyinng@users.noreply.github.com>
2022-08-21 15:36:43 -0300

    Creating a fuctional crawler

---
+++ src/web-scraping/data-pieces/pichau/MB/MotherBoard.py
@@ -0,0 +1,77 @@
+import arrow
+from selenium import webdriver
+
+# Memory specific data
+
+installmentPriceProducts = []  # Memory Installment Prices
+pricesProducts = []  # Memory Prices
+namesProducts = []  # Memory Name
+linksProducts = []  # Memory Links
+imgProducts = []  # Memory Image
+allData = []  # Memory all data
+local = arrow.utcnow()
+
+
+for i in range(3):
+    driver = webdriver.Chrome()
+    page = i + 1
+    link = 'https://www.pichau.com.br/hardware/placa-m-e?page='
+    new_link = link + str(page)
+    driver.get(new_link)
+    height = driver.execute_script("return document.body.scrollHeight")
+    scroll = 0
+    driver.fullscreen_window()
+
+    # Crawling Products == Image
+    product = driver.find_elements('tag name', 'img')
+    for e in product:
+        if 'product' in e.get_attribute('src'):  # Only separate images with product in the name
+            imgProducts.append(e.get_attribute('src'))
+    while scroll < height:
+        driver.execute_script(f"window.scrollTo(0, {scroll});")
+        product = driver.find_elements('tag name', 'img')
+        for e in product:
+            if 'product' in e.get_attribute('src'):  # Only separate images with product in the name
+                imgProducts.append(e.get_attribute('src'))
+        scroll += 200
+    imgProducts = list(dict.fromkeys(imgProducts))
+
+    # Crawling Products == Price
+    product = driver.find_elements('class name', 'jss191')  # Possibles class name = jss191
+    for i in product:
+        if 'R$' in i.text:
+            pricesProducts.append(i.text)
+
+    # Crawling Products == Installment Price
+    product = driver.find_elements('class name', 'jss199')  # Possibles class name = jss199
+    for i in product:
+        if 'R$' in i.text:
+            installmentPriceProducts.append(i.text)
+
+    # Crawling Products == Name
+    product = driver.find_elements('tag name', 'h2')
+    for i in product:
+        if i.text == "":
+            continue
+        namesProducts.append(i.text)
+
+    # Crawling Products == Links
+    links = driver.find_elements('tag name', 'a')
+    for i in links:
+        if 'placa-mae' in i.get_attribute('href'):
+            linksProducts.append(i.get_attribute('href'))
+    driver.close()
+
+# Separating data in dictionary for better reading
+for i in range(len(installmentPriceProducts)):
+    if '.' in pricesProducts[i]:
+        pricesProducts[i] = pricesProducts[i].replace('.', '')
+    changeablePrices = pricesProducts[i].replace('R$', '').replace(',', '.')
+    if '.' in installmentPriceProducts[i]:
+        installmentPriceProducts[i] = installmentPriceProducts[i].replace('.', '')
+    changeableInstallmentPriceProducts = installmentPriceProducts[i].replace('R$', '').replace(',', '.')
+    dataDic = {'Name': namesProducts[i], 'Price': [pricesProducts[i], float(changeablePrices)],
+               'Installment price': [installmentPriceProducts[i], float(changeableInstallmentPriceProducts)],
+               'Link': linksProducts[i], 'Image': imgProducts[i], 'Time': local.format('YYYY-MM-DD HH:mm:ss')}
+    allData.append(dataDic)
+lostData = 0
