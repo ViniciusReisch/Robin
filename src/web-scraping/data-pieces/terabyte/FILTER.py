2a0df9f39ab6c84a355a88018bf31e936e699f38
Vinícius Reisch <97248458+Flyinng@users.noreply.github.com>
2022-08-31 17:01:25 -0300

    Uptade Terabyte

--- src/web-scraping/data-pieces/terabyte/FILTER.py
+++ src/web-scraping/data-pieces/terabyte/FILTER.py
@@ -1,11 +1,11 @@
-from terabyte.RAM import RAM
-from terabyte.CPU import CPU
-from terabyte.GPU import GraphicsCard
-from terabyte.MB import MotherBoard
-from terabyte.HD import HardDisk
-from terabyte.SSD import SSD
-from terabyte.FONT import Font
-from terabyte.CABINET import Cabinet
+# from terabyte.RAM import RAM
+# from terabyte.CPU import CPU
+from GPU import GraphicsCard
+# from terabyte.MB import MotherBoard
+# from terabyte.HD import HardDisk
+# from terabyte.SSD import SSD
+# from terabyte.FONT import Font
+from CABINET import Cabinet
 
 
 # RAM Filter
@@ -317,96 +317,7 @@ class TerabyteGPU:
         allGPU = TerabyteGPU.GPU_get()
 
         # Specific GPU lists
-        Model = {
-            'GT 1030 2GB': [],
-            'GT 610 2GB': [],
-            'GT 710 1GB': [],
-            'GT 710 2GB': [],
-            'GT 730 2GB': [],
-            'GT 730 4GB': [],
-            'GT 240 1GB': [],
-            'GTX 1050 2GB': [],
-            'GTX 1050 3GB': [],
-            'GTX 1050 TI 4GB': [],
-            'GTX 1060 3GB': [],
-            'GTX 1060 6GB': [],
-            'GTX 1070 8GB': [],
-            'GTX 1070 TI 8GB': [],
-            'GTX 1080 8GB': [],
-            'GTX 1080 TI 11GB': [],
-            'GTX 1630 4GB': [],
-            'GTX 1650 4GB': [],
-            'GTX 1650 SUPER': [],
-            'GTX 1660 6GB': [],
-            'GTX 1660 SUPER': [],
-            'GTX 1660 TI 6GB': [],
-            'GTX 750 TI 2GB': [],
-            'GTX 750 TI 4GB': [],
-            'GTX 980 TI 6GB': [],
-            'NVS 810 4GB': [],
-            'PRO W6600 8GB': [],
-            'QUADRO P1000 4GB': [],
-            'QUADRO P2000 5GB': [],
-            'QUADRO P400 2GB': [],
-            'QUADRO P4000 8GB': [],
-            'QUADRO P620 2GB': [],
-            'QUADRO RTX 4000 8GB': [],
-            'QUADRO RTX A2000 12GB': [],
-            'QUADRO RTX A2000 6GB': [],
-            'QUADRO RTX A4000 16GB': [],
-            'QUADRO RTX A4500 20GB': [],
-            'QUADRO RTX A5000 24GB': [],
-            'QUADRO T1000 4GB': [],
-            'QUADRO T1000 8GB': [],
-            'QUADRO T400 2GB': [],
-            'QUADRO T600 4GB': [],
-            'R5 220 2GB': [],
-            'R5 230 1GB': [],
-            'R5 230 2GB': [],
-            'R7 240': [],
-            'RTX 2060 12GB': [],
-            'RTX 2060 6GB': [],
-            'RTX 2060 SUPER 8GB': [],
-            'RTX 2070 8GB': [],
-            'RTX 2070 SUPER 8GB': [],
-            'RTX 2080 8GB': [],
-            'RTX 2080 SUPER 8GB': [],
-            'RTX 2080 TI 11GB': [],
-            'RTX 3050 4GB': [],
-            'RTX 3050 8GB': [],
-            'RTX 3060 12GB': [],
-            'RTX 3060 TI 8GB': [],
-            'RTX 3070 8GB': [],
-            'RTX 3070 TI 8GB': [],
-            'RTX 3080 10GB': [],
-            'RTX 3080 12GB': [],
-            'RTX 3080 TI 12GB': [],
-            'RTX 3090 24GB': [],
-            'RTX 3090 TI 24 GB': [],
-            'RX 460 2GB': [],
-            'RX 550 2GB': [],
-            'RX 550 4GB': [],
-            'RX 5500 4GB': [],
-            'RX 5600 XT 6GB': [],
-            'RX 570 4GB': [],
-            'RX 570 8GB': [],
-            'RX 5700 8GB': [],
-            'RX 5700 XT 8GB': [],
-            'RX 580 4GB': [],
-            'RX 580 8GB': [],
-            'RX 590 8GB': [],
-            'RX 6400 4GB': [],
-            'RX 6500 XT 4GB': [],
-            'RX 6600 8GB': [],
-            'RX 6600 XT 8GB': [],
-            'RX 6650 XT 8GB': [],
-            'RX 6700 XT 12GB': [],
-            'RX 6750 XT 12GB': [],
-            'RX 6800 16GB': [],
-            'RX 6800 XT 16GB': [],
-            'RX 6900 XT 16GB': [],
-            'RX 6950 XT 16GB': []
-        }
+
         allModel = [
             'GT 1030', 'GT 610', 'GT 710 1GB', 'GT 710 2GB', 'GT 730', 'GT 730',
             'GT240', 'GTX 1050 2GB', 'GTX 1050 3GB', 'GTX 1050 TI', 'GTX 1060',
@@ -426,14 +337,12 @@ class TerabyteGPU:
             'RX 6600', 'RX 6600 XT', 'RX 6650 XT', 'RX 6700 XT', 'RX 6750 XT',
             'RX 6800', 'RX 6800 XT', 'RX 6900 XT', 'RX 6950 XT'
         ]
-        key = list(Model.values())
         for i in range(len(allModel)):
             for data in allGPU:
                 if allModel[i] in data['Name']:
-                    key = list(Model.values())
-                    key[i].append(data)
+                    data.update({'Model': allModel[i]})
 
-        return Model, allGPU
+        return allGPU
 
 
 # Mother Board Filter
@@ -680,58 +589,24 @@ class TerabyteCabinet:
         allCabinet = TerabyteCabinet.Cabinet_get()
         # Specific Case lists
 
-        # Case Types
-        Types = {
-            'Full-Tower': [],
-            'Mid-Tower': [],
-            'Mini-Tower': []
-        }
-
-        # Case Colors
-        Colors = {
-            'Azul': [],
-            'Branco': [],
-            'Branco - Preto': [],
-            'Cinza': [],
-            'Prata': [],
-            'Preto': [],
-            'Preto - Azul': [],
-            'Preto - Branco': [],
-            'Preto - Laranja': [],
-            'Preto - Prata': [],
-            'Preto - Vermelho': [],
-            'Rosa': [],
-            'Verde': []
-        }
         # Filters
-
-        # Case Types
-        allTypes = ['Full Tower', 'Mid Tower', 'Mini Tower']
-        key = list(Types.values())
-        for i in range(len(allTypes)):
-            for data in allCabinet:
-                if allTypes[i] in data['Name']:
-                    key = list(Types.values())
-                    key[i].append(data)
 
         # Case Colors
         allColors = ['Blue', 'White', 'White/Black', 'Gray', 'Prata', 'Black', 'Preto/Azul',
                      'Black/White', 'Preto/Laranja', 'Black/Gray', 'Red', 'Pink', 'Green']
-        key = list(Colors.values())
         for i in range(len(allColors)):
             for data in allCabinet:
                 if allColors[i] in data['Name']:
-                    key = list(Colors.values())
-                    key[i].append(data)
+                    data.update({'Model': allColors[i]})
 
-        return Types, Colors, allCabinet
+        return allCabinet
 
 
-TerabyteCabinet_Types, TerabyteCabinet_Colors, TerabyteAllCabinet = TerabyteCabinet.Cabinet_FILTERS()
-TerabyteFont_Potency, TerabyteAllFont = TerabyteFont.Font_FILTERS()
-TerabyteSSD_Interface, TerabyteSSD_Format, TerabyteSSD_Capacity, TerabyteAllSSD = TerabyteSSD.SSD_FILTERS()
-TerabyteHD_Capacity, TerabyteAllHD = TerabyteHD.HD_FILTERS()
-TerabyteMB_DDR, TerabyteMB_Format, TerabyteMB_Socket, TerabyteAllMB = TerabyteMotherBoard.MB_FILTERS()
-TerabyteGPU_Model, TerabyteAllGPU = TerabyteGPU.GPU_FILTERS()
-TerabyteCPU_Socket, TerabyteCPU_Platform, TerabyteAllCPU = TerabyteCPU.CPU_FILTERS()
-TerabyteRAM_DDR, TerabyteRAM_capacityDDR5, TerabyteRAM_capacityDDR4, TerabyteRAM_capacityDDR3, TerabyteRAM_frequencyDDR5, TerabyteRAM_frequencyDDR4, TerabyteRAM_frequencyDDR3, TerabyteAllRAM = TerabyteRAM.RAM_FILTERS()
+TerabyteAllCabinet = TerabyteCabinet.Cabinet_FILTERS()
+# TerabyteFont_Potency, TerabyteAllFont = TerabyteFont.Font_FILTERS()
+# TerabyteSSD_Interface, TerabyteSSD_Format, TerabyteSSD_Capacity, TerabyteAllSSD = TerabyteSSD.SSD_FILTERS()
+# TerabyteHD_Capacity, TerabyteAllHD = TerabyteHD.HD_FILTERS()
+# TerabyteMB_DDR, TerabyteMB_Format, TerabyteMB_Socket, TerabyteAllMB = TerabyteMotherBoard.MB_FILTERS()
+TerabyteAllGPU = TerabyteGPU.GPU_FILTERS()
+# TerabyteCPU_Socket, TerabyteCPU_Platform, TerabyteAllCPU = TerabyteCPU.CPU_FILTERS()
+# TerabyteRAM_DDR, TerabyteRAM_capacityDDR5, TerabyteRAM_capacityDDR4, TerabyteRAM_capacityDDR3, TerabyteRAM_frequencyDDR5, TerabyteRAM_frequencyDDR4, TerabyteRAM_frequencyDDR3, TerabyteAllRAM = TerabyteRAM.RAM_FILTERS()
