9ae538fd1ae761ab71988edc8ae37658ff71f96d
Vinícius Reisch <97248458+Flyinng@users.noreply.github.com>
2022-09-14 08:28:55 -0300

    Update main.py

--- src/twitter-bot/web-scraping/main.py
+++ src/twitter-bot/web-scraping/main.py
@@ -2,6 +2,8 @@ import arrow
 from selenium import webdriver
 
 passPricesProducts = []
+priceTag = 0
+passPriceTag = 0
 pricesProducts = []             # Memory Prices
 namesProducts = []              # Memory Name
 linksProducts = []              # Memory Links
@@ -18,24 +20,29 @@ driver.fullscreen_window()
 # Crawling Products == Name
 product = driver.find_element('class name', 'jss59')
 names = product.find_elements('class name', 'MuiTypography-h6')
-prices = product.find_elements('class name', 'jss145')
-passPrices = product.find_elements('class name', 'jss154')
+
+prices = product.find_elements('class name', 'jss145')  # jss224, jss145
+priceTagList = ['jss224', 'jss139']
+while len(prices) == 0:
+    prices = product.find_elements('class name', priceTagList[priceTag])
+    priceTag += 1
+
+passPrices = product.find_elements('class name', 'jss154')  # jss233, jss154
+passPriceTagList = ['jss233', 'jss148']
+while len(prices) == 0:
+    passPrices = product.find_elements('class name', passPriceTagList[passPriceTag])
+    passPriceTag += 1
+
 img = product.find_elements('tag name', 'img')
 links = product.find_elements('tag name', 'a')
 
 for i in names:
-    if i.text == "":
-        continue
     namesProducts.append(i.text)
 
 for i in prices:
-    if i.text == "":
-        continue
     pricesProducts.append(i.text)
 
 for i in passPrices:
-    if i.text == "":
-        continue
     passPricesProducts.append(i.text)
 
 while scroll < height:
@@ -51,13 +58,20 @@ for i in links:
     linksProducts.append(i.get_attribute('href'))
 
 for i in range(len(pricesProducts)):
-    if '.' in pricesProducts[i]:
-        pricesProducts[i] = pricesProducts[i].replace('.', '')
-    changeablePrices = pricesProducts[i].replace('R$', '').replace(',', '.')
-    if '.' in passPricesProducts[i]:
-        passPricesProducts[i] = passPricesProducts[i].replace('.', '')
-    changeablePassedPrices = passPricesProducts[i].replace('de R$ ', '').replace(',', '.').replace(' por:', '')
+    if passPricesProducts[i] is not None:
+        if '.' in pricesProducts[i]:
+            pricesProducts[i] = pricesProducts[i].replace('.', '')
+        changeablePrices = pricesProducts[i].replace('R$', '').replace(',', '.')
+        if '.' in passPricesProducts[i]:
+            passPricesProducts[i] = passPricesProducts[i].replace('.', '')
+        changeablePassedPrices = passPricesProducts[i].replace('de R$ ', '').replace(',', '.').replace(' por:', '')
+        discountProduct = ((float(changeablePassedPrices) - float(changeablePrices)) * 100) / float(changeablePassedPrices)
     dataDic = {'Store': 'Pichau', 'Name': namesProducts[i], 'Price': [pricesProducts[i], float(changeablePrices)],
                'Passed Prices': [passPricesProducts[i], float(changeablePassedPrices)],
-               'Link': linksProducts[i], 'Image': imgProducts[i], 'Time': local.format('YYYY-MM-DD HH:mm:ss')}
+               'Link': linksProducts[i], 'Image': imgProducts[i], 'Time': local.format('YYYY-MM-DD HH:mm:ss'),
+               'Discount': str(round(discountProduct, 2)) + '%'}
+    text = f'{namesProducts[i]} com desconto de {str(round(discountProduct, 2))}% de R${changeablePassedPrices} por R${changeablePrices}'
+    print(text)
+
     allData.append(dataDic)
+driver.close()
