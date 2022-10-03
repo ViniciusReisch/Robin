4547ceb7672f1adee772b21d762989144da68663
Gabriel Eduardo Ritter <97248458+Flyinng@users.noreply.github.com>
2022-10-03 11:49:34 -0300

    Creating the product cards, with django filter.
    Using bootstrap temporarily to gather all products in the /allproducts url

--- src/front/products/admin.py
+++ src/front/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Alldata
+
+admin.site.register(Alldata)
 
 # Register your models here.
--- src/front/products/views.py
+++ src/front/products/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Alldata
 
 def index(request):
     return render(request, 'products/index.html')
 
-# Create your views here.
+def allProducts(request):
+    products = Alldata.objects.all()
+    return render(request, 'products/allProducts.html',
+                  {'products': products})
+
